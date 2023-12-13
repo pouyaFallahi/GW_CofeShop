251da02f08d462ba89c9a3d8c744fe72e573e307
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-12-13 21:23:51 +0330

    UPDATE | modigied setting for postgersql

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_coffe_shop',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
+
 }
 
 
