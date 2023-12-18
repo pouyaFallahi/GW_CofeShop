f27ce744d9494224bdaf65c6ecd9f030c0f6087e
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-12-18 10:05:12 +0330

    UPDATE | Fix the error AbstractBaseUser.set_password() missing

--- coffe/authentication.py
+++ coffe/authentication.py
@@ -2,17 +2,19 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-
 class CustomAuthenticate(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-
         try:
             user = get_user_model().objects.get(
                 Q(username__iexact=username) | Q(phone__iexact=username) | Q(email__iexact=username))
         except get_user_model().DoesNotExist:
-            # Run the default password hasher once to reduce the timing
-            # difference between an existing and a nonexistent user (#20760).
-            get_user_model().set_password(password)
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
+            # Create a new user instance and set the password
+            user = get_user_model()(
+                username=username,
+                email=username,  # You may need to adjust this based on your user model
+            )
+            user.set_password(password)
+            user.save()
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
