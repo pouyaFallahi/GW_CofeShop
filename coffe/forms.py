caf8177795783d1b04aeaeffbf0f1a1806009e9c
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-12-19 15:36:32 +0330

    FIX | fix bug login

--- coffe/forms.py
+++ coffe/forms.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
 
 from .models import MyUser
 from django import forms
@@ -15,3 +19,8 @@ class CustomerCreationModelForm(ModelForm):
     class Meta:
         model = MyUser
         fields = ['first_name', 'last_name', 'gender', 'email', 'phone', 'username', 'password', 'address']
+
+
+class LoginUser(View):
+    template_name = 'login.html'
+    error_message = forms.CharField(widget=forms.HiddenInput(), required=False)
