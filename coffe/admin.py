a22cfcf4e5aec9b5b9127e955fa799024e1a51af
pouyaFallahi <pouya.fallahi80@gmail.com>
2023-12-19 13:44:44 +0330

    ADD | add category, item in admin.site

--- coffe/admin.py
+++ coffe/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import MyUser
+from .models import MyUser, Category, Item
 # Register your models here.
 admin.site.register(MyUser)
+admin.site.register(Category)
+admin.site.register(Item)
