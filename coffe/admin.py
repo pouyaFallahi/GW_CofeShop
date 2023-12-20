4fde517cd1124a0d641591cbc3b5dfddb7cb8411
fa-mahabadi <144953741+fa-mahabadi@users.noreply.github.com>
2023-12-20 13:53:55 +0330

    Update |admin

--- coffe/admin.py
+++ coffe/admin.py
@@ -3,4 +3,12 @@ from .models import MyUser, Category, Item
 # Register your models here.
 admin.site.register(MyUser)
 admin.site.register(Category)
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'image_preview']
+    readonly_fields=['thumbnail_preview']
+    def thumbnail_preview(self,obj):
+        return obj.thumbnail_preview
+    thumbnail_preview.short_description='preview'
+    thumbnail_preview.allow_tags=True
+    
+admin.site.register(Item, ItemAdmin)
